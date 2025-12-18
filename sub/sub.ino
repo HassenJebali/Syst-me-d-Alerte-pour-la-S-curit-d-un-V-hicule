f42f0dcfd53d9181f8dc0ef150c0f4d853475e52
jebali.hassen <hdjebali92@gmail.com>
2025-12-18 16:11:33 +0100

    configuration MQTT

---
+++ sub/sub.ino
@@ -0,0 +1,72 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Informations WiFi
+const char* ssid = "TON_SSID";
+const char* password = "TON_MDP_WIFI";
+
+// Broker MQTT
+const char* mqtt_server = "172.24.153.231";  // IP de ton Ubuntu
+const int mqtt_port = 1883;
+const char* topic = "Système d'Alerte pour la Sécurité d'un Véhicule";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connexion à ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connecté");
+  Serial.println("IP : ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message reçu [");
+  Serial.print(topic);
+  Serial.print("] : ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connexion au broker MQTT...");
+    if (client.connect("ESP32_Subscriber")) {
+      Serial.println("Connecté !");
+      client.subscribe(topic);
+    } else {
+      Serial.print("Échec, rc=");
+      Serial.print(client.state());
+      Serial.println(" Nouvelle tentative dans 5s");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
